b8bc12f167e825c2af30ba50aeb2564e59b6a021
yaoyu bao <yaobao5354@gmail.com>
2019-08-26 21:53:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 	  railroads: { 
 	    pieces: 4, 
 	    names: {}, 
-	    rent_in_dollars: {one_piece_owned: 25}
+	    rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50}
 	  }
 	}
 end
